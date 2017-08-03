38109f2d6f20a6b706c6f96b91557d8956b5c0ea
Tair Assimov <tair@assimov.net>
2017-08-03 09:28:54 +0200

    Add YouTube search service with injectables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
+import { youTubeSearchInjectables } from './youtube-search/youtube-search-injectables';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +11,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    youTubeSearchInjectables
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
