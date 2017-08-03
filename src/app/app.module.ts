1bd4fc2353a40aa904890a32a66f3244b055353b
Tair Assimov <tair@assimov.net>
2017-08-03 09:36:11 +0200

    Add YouTube search box component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { youTubeSearchInjectables } from './youtube-search/youtube-search-injectables';
+import { YouTubeSearchBoxComponent } from './youtube-search/youtube-search-box.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    YouTubeSearchBoxComponent
   ],
   imports: [
     BrowserModule,
